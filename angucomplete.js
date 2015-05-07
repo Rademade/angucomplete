7d4e8c5b41f360fb56b1bdf25be5e1a0d27e5d4b
andrey-abramow <abramow.andrey@gmail.com>
2015-05-07 11:13:49 +0300

    fixing bugs while aglifying

--- angucomplete.js
+++ angucomplete.js
@@ -5,7 +5,7 @@
  */
 
 angular.module('angucomplete', [] )
-    .directive('angucomplete', function ($parse, $http, $sce, $timeout) {
+    .directive('angucomplete', [ '$parse', '$http', '$sce', '$timeout', function ($parse, $http, $sce, $timeout) {
         return {
             restrict: 'EA',
             scope: {
@@ -258,5 +258,5 @@ angular.module('angucomplete', [] )
 
             }
         };
-    });
+    }]);
 
